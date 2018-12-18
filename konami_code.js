75b0b04b922bc6488fd647e3c964843d49694e96
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-12-18 19:58:35 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,16 +14,29 @@ const codes = [
 function init() {
   // your code here
   let idx = 0
+<<<<<<< HEAD
 
   document.body.addEventListener("keydown", (e) => {
   const key = e.key
 
   idx = (codes[idx] === key) ? ++idx : 0
 
+=======
+ 
+  document.body.addEventListener("keydown", (e) => {
+  const key = e.key
+  
+  idx = (codes[idx] === key) ? ++idx : 0
+  
+>>>>>>> 66b5f1ea6853edb587fb099835c084d775b48fdf
   if (idx === codes.length) {
     window.alert("Hurray!");
     idx = 0
   }
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> 66b5f1ea6853edb587fb099835c084d775b48fdf
 });
 }
